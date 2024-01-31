f673158692b2ad0c730b3294dbc5eb80216a8eef
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2024-01-31 15:03:11 -0500

    Added enemy, no movement yet

---
+++ Chalkboard-Project2 (1)/Assets/Scripts/KillerChase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerChase : MonoBehaviour
+{
+    public Transform player;
+
+    void Update()
+    {
+        
+    }
+}
