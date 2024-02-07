c3ca5c421a674651fbe4979c4a845bc096162401
LukTeg <lukasgrat@gmail.com>
2024-02-07 18:10:25 -0500

    Scene transition

---
+++ Chalkboard Project3/Assets/Scripts/SceneChangeTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if this breaks, just loadScene(1)
+        // im abstracting for no good reason
+        //SceneManager.LoadScene(other.gameObject.scene.buildIndex + 1);
+        SceneManager.LoadScene(1);
+    }
+}
