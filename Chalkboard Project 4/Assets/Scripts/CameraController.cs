3dcbbc4d9fe9cf4cfbde2e789a87f47b71ad1be2
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2024-02-15 11:56:04 -0500

    Added player controller and bench

---
+++ Chalkboard Project 4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static float pitch;
+    public static float yaw;
+    public Transform camTrans;
+    public Transform playerTrans;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        yaw = camTrans.rotation.eulerAngles.y;
+        offset = camTrans.position - playerTrans.position;
+    }
+
+    void Update()
+    {
+        //sets the camera's position to follow the player
+        camTrans.position = playerTrans.position + offset;
+
+        //rotates the camera and player according to mouse input
+        pitch -= Input.GetAxis("Mouse Y");
+        yaw += Input.GetAxis("Mouse X");
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        while (yaw < 0f) { yaw += 360f; }
+        while (yaw >= 360f) { yaw -= 360f; }
+
+        camTrans.eulerAngles = new Vector3(pitch, yaw, 0);
+        playerTrans.eulerAngles = new Vector3(0, yaw, 0);
+    }
+}
