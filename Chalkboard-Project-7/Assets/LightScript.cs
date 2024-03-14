085803dd0d4bfcdc17b15c6831703aeba79a6bc5
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2024-03-14 12:27:07 -0400

    Edited window light system

--- Chalkboard-Project-7/Assets/LightScript.cs
+++ Chalkboard-Project-7/Assets/LightScript.cs
@@ -4,35 +4,36 @@ using UnityEngine;
 
 public class LightScript : MonoBehaviour
 {
-    float timer;
+    public float timer;
+
     // Start is called before the first frame update
     void Start()
     {
         int setup = Random.Range(1, 8);
-        if (setup < 3)
-        {
-            this.gameObject.SetActive(false);
-        }
-        else
-        {
-            timer = Random.Range(5, 15);
-        }
+        this.gameObject.SetActive(setup > 4);
+
+        timer = Random.Range(10, 60);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (this.timer == 0)
+        if (timer > 0)
         {
-            this.gameObject.SetActive(false);
+            timer -= Time.deltaTime;
         }
-        else if (this.timer < Time.deltaTime)
+        if (this.timer <= 0)
         {
-            this.timer = 0;
-        }
-        else
-        {
-            this.timer -= Time.deltaTime;
+            if (gameObject.activeSelf)
+            {
+                this.gameObject.SetActive(false);
+                timer = Random.Range(20, 40);
+            }
+            else
+            {
+                this.gameObject.SetActive(true);
+                timer = Random.Range(5, 15);
+            }
         }
     }
 }
