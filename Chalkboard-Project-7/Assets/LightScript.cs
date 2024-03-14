72e4ca746fdb1c6d5d7b57a5d230456750a687d3
LukTeg <lukasgrat@gmail.com>
2024-03-14 09:56:01 -0400

    Lighting tweek

---
+++ Chalkboard-Project-7/Assets/LightScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int setup = Random.Range(1, 4);
+        Debug.Log(setup);
+        if (setup < 2)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            timer = Random.Range(5, 15);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.timer == 0)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else if (this.timer < Time.deltaTime)
+        {
+            this.timer = 0;
+        }
+        else
+        {
+            this.timer -= Time.deltaTime;
+        }
+    }
+}
